8bb6f61db6338ec80f3732cd6fca2d8cd6530d79
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-03-10 17:55:25 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -12,6 +12,12 @@ constructor(){
           this.wrong=createElement("h4");
     this.b=createButton("2");
           this.rigth=createElement("h4");
+
+
+    this.question2=createElement("h4");     
+    this.op1=createElement("h4");
+    this.op2=createElement("h4");
+
 }
 display(){
 
@@ -43,48 +49,46 @@ contestant.updateName();
 this.a.mousePressed(()=>{
 this.wrong.html("wrong you lose (X_X)");
 this.wrong.position(100,300);
+
 })
 
 this.b.mousePressed(()=>{
 this.rigth.html("yes You are rigth(O_O)");
 this.rigth.position(100,300)
+
+this.a1=createButton("1");
+this.r1=createElement("h4");
+this.b1=createButton("2");
+this.w1=createElement("h4");
+
+this.question.hide();
+this.option1.hide();
+this.option2.hide();
+this.rigth.hide();
+
+this.question2.html("Question2 :-will you tell me your phone's passward 🤔🤔 ?");
+this.question2.position(100,80);
+this.op1.html("1:yes");
+this.op1.position(150,160);
+this.op2.html("2:no");
+this.op2.position(150,120);
+
+this.a1.mousePressed(()=>{
+this.w1.html("wrong you lose (X_X)");
+this.w1.position(100,300);
+
+})
+
+this.b1.mousePressed(()=>{
+
+this.r1.html("yes You are rigth(O_O)");
+this.r1.position(100,300);
+
+})
+
+
 })
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
